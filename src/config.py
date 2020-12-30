c702d2ca320cba7ac3195a9b197aa21b66c15c6d
Morgan Funtowicz <funtowiczmo@gmail.com>
2020-12-30 17:11:27 +0100

    Save transformers version as part of the benchmark

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from logging import getLogger
 
 from omegaconf import MISSING
+from transformers import __version__ as transformers_version
 
 from backends import BackendConfig
 
@@ -11,6 +12,7 @@ LOGGER = getLogger("benchmark")
 
 @dataclass()
 class BenchmarkConfig:
+    transformers_version: str = transformers_version
     num_runs: int = MISSING
     warmup_runs: int = MISSING
     model: str = MISSING
