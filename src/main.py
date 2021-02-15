585083bd52e71999e474464e04ecf629174f0696
Morgan Funtowicz <funtowiczmo@gmail.com>
2021-02-15 18:22:42 +0100

    Handle Transparent Huge Pages on bash side.
    Signed-off-by: Morgan Funtowicz <funtowiczmo@gmail.com>

--- src/main.py
+++ src/main.py
@@ -27,9 +27,6 @@ from backends.pytorch import PyTorchConfig
 from backends.tensorflow import TensorflowConfig
 from config import BenchmarkConfig
 
-# Transparent Huge Pages default location
-DEBIAN_TRANSPARENT_PAGES_PATH = "/sys/kernel/mm/transparent_hugepage/enabled"
-REDHAT_TRANSPARENT_PAGES_PATH = "/sys/kernel/mm/redhat_transparent_hugepage/enabled"
 
 # Register configurations
 cs = ConfigStore.instance()
@@ -41,30 +38,9 @@ cs.store(group="backend", name="xla", node=TensorflowConfig)
 cs.store(group="backend", name="ort", node=OnnxRuntimeConfig)
 
 
-def use_transparent_huge_page(enable: bool):
-    # Look for common TBH places
-    if exists(DEBIAN_TRANSPARENT_PAGES_PATH):
-        tbh_config_path = DEBIAN_TRANSPARENT_PAGES_PATH
-    elif exists(REDHAT_TRANSPARENT_PAGES_PATH):
-        tbh_config_path = REDHAT_TRANSPARENT_PAGES_PATH
-    else:
-        print("Unable to locate Transparent Huge Page configuration files on your system, no action will be taken.")
-        return
-
-    print(f"Found Transparent Huge Page configuration files at: {tbh_config_path}")
-
-    # Turn on (always) / off (never) TBH
-    thb_status = "always" if enable else "never"
-    system(f"echo {thb_status} > {tbh_config_path}")
-    print(f"Transparent Huge Page enabled: {system(f'cat {tbh_config_path}')}")
-
-
 @hydra.main(config_path="../configs", config_name="benchmark")
 def run(config: BenchmarkConfig) -> None:
     print(OmegaConf.to_yaml(config))
-
-    # Turn on/off usage of Transparent Huge Pages
-    use_transparent_huge_page(config.use_huge_page)
 
     backend_factory: Type[Backend] = get_class(config.backend._target_)
     backend = backend_factory.allocate(config)
