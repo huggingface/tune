f34af1637060a7bc2a9d71f79c181ea85ed15d8e
Morgan Funtowicz <funtowiczmo@gmail.com>
2021-02-17 15:22:42 +0100

    Expose memory allocation library through the framework.

--- src/main.py
+++ src/main.py
@@ -11,21 +11,22 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-
-from os import system
-from os.path import exists
+from os import environ
+from pathlib import Path
 from typing import Type
 
 import hydra
 from hydra.core.config_store import ConfigStore
 from hydra.utils import get_class
-from omegaconf import OmegaConf
 
 from backends import Backend
 from backends.ort import OnnxRuntimeConfig
 from backends.pytorch import PyTorchConfig
 from backends.tensorflow import TensorflowConfig
 from config import BenchmarkConfig
+
+# Environment variables constants
+ENV_VAR_TCMALLOC_LIBRARY_PATH = "TCMALLOC_LIBRARY_PATH"
 
 
 # Register configurations
@@ -38,9 +39,25 @@ cs.store(group="backend", name="xla_backend", node=TensorflowConfig)
 cs.store(group="backend", name="ort_backend", node=OnnxRuntimeConfig)
 
 
+def check_tcmalloc(config: BenchmarkConfig):
+    if ENV_VAR_TCMALLOC_LIBRARY_PATH not in environ:
+        raise ValueError(f"Env var {ENV_VAR_TCMALLOC_LIBRARY_PATH} has to be set to location of libtcmalloc.so")
+
+    if len(environ[ENV_VAR_TCMALLOC_LIBRARY_PATH]) == 0:
+        raise ValueError(f"Env var {ENV_VAR_TCMALLOC_LIBRARY_PATH} cannot be empty")
+
+    tcmalloc_path = Path(environ[ENV_VAR_TCMALLOC_LIBRARY_PATH])
+    if not tcmalloc_path.exists():
+        raise ValueError(
+            f"Path {tcmalloc_path.as_posix()} pointed by "
+            f"env var {ENV_VAR_TCMALLOC_LIBRARY_PATH} doesn't exist"
+        )
+
+
 @hydra.main(config_path="../configs", config_name="benchmark")
 def run(config: BenchmarkConfig) -> None:
-    print(OmegaConf.to_yaml(config))
+    if hasattr(config, "malloc") and "tcmalloc" == config.malloc.name:
+        check_tcmalloc(config)
 
     backend_factory: Type[Backend] = get_class(config.backend._target_)
     backend = backend_factory.allocate(config)
