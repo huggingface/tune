d42cc7c2e99c0b5455d934615c9fd92d3af77eef
Pradeep Sakhamoori <pradeep_sto@yahoo.com>
2021-11-09 11:43:50 -0700

    Add instance_id to results csv
    Assign instance_id value based on outputs folder directory time stamp(s)

--- scripts/launch_results_parser.py
+++ scripts/launch_results_parser.py
@@ -33,19 +33,29 @@ def main():
     else:
         root_path = args.dirpath
 
+    l_root_path = len(os.path.normpath(root_path).split(os.path.sep))
+    idx = 0
+   
+    root_instance_dir = []
+
+    for ts, folder in sorted([(os.path.getctime(root_path + dir),root_path + dir) for dir in os.listdir(root_path)]):
+        root_instance_dir.append(os.path.normpath(folder).split(os.path.sep)[-1].lower())
+    #print("root_instance_dir =", root_instance_dir)
+
     for curr_dir, list_dirs, file_names in os.walk(root_path):
+        l_curr_dir = len(os.path.normpath(curr_dir).split(os.path.sep))
+        if l_curr_dir == (l_root_path + 1):
+            curr_dir_name = os.path.normpath(curr_dir).split(os.path.sep)[-1]
+            idx = root_instance_dir.index(curr_dir_name) + 1
+        
         for f in file_names:
             curr_dir_split = os.path.normpath(curr_dir).split(os.path.sep)
-            if len(curr_dir_split) == 7:
-                instance = curr_dir_split[-3]
-            else:
-                instance = curr_dir_split[-2]
             f_ext = os.path.splitext(f)[-1].lower()
             if f_ext == ".csv":
                fn = f.split('-')
                f_csv = os.path.join(curr_dir, f)
                df_csv = pd.read_csv(f_csv)
-               df_csv['instance_id'] = [int(instance)]
+               df_csv['instance_id'] = [idx]
                df_ = df_.append(df_csv)
     for i, s in enumerate(fn):
         if i == 0:
