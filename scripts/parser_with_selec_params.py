eac82dc6054b6d58ca1006c38ab0b24748f3b9a3
Pradeep Sakhamoori <pradeep_sto@yahoo.com>
2021-11-19 16:35:18 -0700

    Update parser_with_selec_params.py

--- scripts/parser_with_selec_params.py
+++ scripts/parser_with_selec_params.py
@@ -1,5 +1,17 @@
-#!/usr/bin/env python
-# Copyright (c) 2014, Intel Corporation.
+#!/bin/bash
+# Copyright (c) 2019 Intel Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Pradeep, Sakhamoori <pradeep.sakhamoori@intel.com>
+# Date : 11/10/2021
 
 import pandas as pd
 import sys, os
@@ -7,9 +19,7 @@ from argparse import ArgumentParser, SUPPRESS
 
 NUM_OF_INSTANCE = [1, 2, 3, 4]  # Mapped values of (1, 2, 4, 8)
 SEQ_LENGTH = [20, 32, 128, 384, 512]
-#BATCH_SIZE = [1, 2, 4, 8]
 BATCH_SIZE = [8, 4, 2, 1]
-#NUM_THREADS = [8, 16, 32, 64]
 BACK_END = ['pytorch', 'openvino', 'tensorflow']
 
 def build_argparser():
